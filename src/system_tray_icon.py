07c26dbc517f387793756d4e50bdd035d4d301d8
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2025-12-17 20:53:34 -0300

    Add copy to clipboard button

--- src/system_tray_icon.py
+++ src/system_tray_icon.py
@@ -26,29 +26,46 @@ class SystemTrayIcon:
         dc.ellipse((width//6, height//6, 5*width//6, 5*height//6), fill=color)
         return image
 
-    def on_clicked(self, icon, item):
+    def create_copy_menu_item(self, transcription):
+        def copy_to_clipboard(icon, item):
+            pyperclip.copy(transcription)
+
+        if len(transcription) <= 10:
+            display_text = f'Copy "{transcription}"'
+        else:
+            display_text = f'Copy "{transcription[:15]}..."'
+
+        return pystray.MenuItem(display_text, copy_to_clipboard)
+
+    def record_or_transcribe(self, icon, item):
         try:
             self.is_recording = not self.is_recording
             color = 'yellow'
             self.icon.icon = self.create_image(color)
+            transcription = ''
 
             if self.is_recording:
                 self.recorder.start()
             else:
                 self.recorder.stop()
-                text = self.recorder.transcribe()
-                print(text)
-                pyperclip.copy(text)
+                transcription = self.recorder.transcribe().strip()
+                print(transcription)
+                pyperclip.copy(transcription)
 
             color = 'red' if self.is_recording else 'blue'
             self.icon.icon = self.create_image(color)
             text = UiText.STOP if self.is_recording else UiText.RECORD
-            self.icon.menu = pystray.Menu(
-                pystray.MenuItem(text, self.on_clicked),
-                pystray.MenuItem(UiText.QUIT, lambda icon, item: self.icon.stop())
-            )
+
+            menu_items = [pystray.MenuItem(text, self.record_or_transcribe)]
+
+            if transcription:
+                menu_items.append(self.create_copy_menu_item(transcription))
+
+            menu_items.append(pystray.MenuItem(UiText.QUIT, lambda icon, item: self.icon.stop()))
+
+            self.icon.menu = pystray.Menu(*menu_items)
         except Exception as e:
-            print('on_clicked error', e)
+            print('record_or_transcribe error', e)
 
     def show(self):
         self.icon = pystray.Icon(
@@ -56,7 +73,7 @@ class SystemTrayIcon:
             title=UiText.TITLE,
             icon=self.create_image('blue'),
             menu=pystray.Menu(
-                pystray.MenuItem(UiText.RECORD, self.on_clicked),
+                pystray.MenuItem(UiText.RECORD, self.record_or_transcribe),
                 pystray.MenuItem(UiText.QUIT, lambda icon, item: self.icon.stop())
             ))
         self.icon.run()
